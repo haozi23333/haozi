15849dd8075b0ace35cbc2860f87a74f5ba7d5d0
haozi <i@haozi.moe>
2019-08-25 10:46:23 +0800

    增加了一个点击特效, 社会主义核心价值观

--- themes/vexo/source/js/script.js
+++ themes/vexo/source/js/script.js
@@ -100,4 +100,49 @@
       catalog.removeClass('fixed-toc')
     }
   })
+
 })(jQuery)
+
+/**
+ * 这段代码来自
+ * https://juejin.im/post/5c6d20b151882562934c9962
+ */
+const T_color = "";//字体颜色,你不设置就是随机颜色,
+
+const T_size = [10, 20];//文字大小区间,不建议太大
+
+const T_font_weight = "bold";//字体粗斜度-->normal,bold,900
+
+const AnimationTime = 1500;//文字消失总毫秒
+
+const Move_up_Distance = 388;//文字移动距离,正数代表上移，反之下移
+
+let a_index = 0;
+$("html").click(function(e){
+  var a = ["富强", "民主", "文明", "和谐", "自由", "平等", "公正" ,"法治", "爱国", "敬业", "诚信", "友善"];
+  var $i = $("<span/>").text(a[a_index]);
+  a_index = (a_index + 1) % a.length;
+  const x = e.pageX, y = e.pageY;
+  let x_color = "#" + ('00000' + (Math.random() * 0x1000000 << 0).toString(16)).substr(-6);//-->随机颜色
+  //console.log(x_color);
+  if(T_color.length>=4){
+    x_color = T_color;
+  }
+
+  var x_size = Math.random()*(T_size[1]-T_size[0]) + T_size[0];
+  x_size +=  "px";
+  $i.css({
+    "z-index": 99999,
+    "top": y - 20,
+    "left": x,
+    "position": "absolute",
+    "font-weight": "bold",
+    "font-size":x_size,
+    "color": x_color,
+    "font-family": "'Source Sans Pro', 'Helvetica Neue', Arial, sans-serif"
+  });
+  $("html").append($i);
+  $i.animate({"top": y-Move_up_Distance,"opacity": 0},AnimationTime,function() {
+    $i.remove();
+  });
+});
