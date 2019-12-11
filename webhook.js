730bcac8b85fa315570889d872e9d2ba5d19eaa5
haozi23333 <i@haozi.moe>
2019-12-11 21:37:16 +0800

    更新 webhook.js

--- webhook.js
+++ webhook.js
@@ -9,14 +9,18 @@ let   base_message = new Date().toLocaleString() + '站点 haozi.moe '
 http.createServer(function (req, res) {
         // 该路径与WebHooks中的路径部分需要完全匹配，实现简易的授权认证。
         if(req.url === '/rebuild'){
-        // 如果url匹配，表示认证通过，则执行 sh ./deploy.sh
+                // 如果url匹配，表示认证通过，则执行 sh ./deploy.sh
                 exec('sh ./deploy.sh', (error, stdout, stderr) => {
                         let notification = base_message + '已部署完毕'
                         if (error) {
                                 notification = base_message + '部署失败 \n' + stderr
                         }
-                        axios.get(`https://api.telegram.org/bot${bot_token}/sendMessage?chat_id=${chat_id}&text=${encodeURIComponent(base_message)}`).then(v => console.log('ok'))
+                        axios.get(`https://api.telegram.org/bot${bot_token}/sendMessage?chat_id=${chat_id}&text=${encodeURIComponent(notification)}`).then(v => console.log('ok'))
                 })
         }
+
+        res.writeHead(200, {"Content-Type": "application/json;charset=utf-8"});
+        res.write(JSON.stringify({ '确实': '确实' }));
         res.end()
+
 }).listen(4002)
